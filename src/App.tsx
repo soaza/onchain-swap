38ca8434d2341f5bf7a568339666a19de7ce75d8
soaza <chukimguan@gmail.com>
2025-08-03 23:15:34 +0800

    feat: init packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import "./App.css";
 
 function App() {
-  return <div className=" text-3xl">Hello</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button variant={"destructive"}>Hello</Button>
+    </div>
+  );
 }
 
 export default App;
